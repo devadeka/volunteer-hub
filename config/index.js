dae20cc23a84aa45042a9e4b22f80f20189ebe40
Stephen Watts <sgwatts96@gmail.com>
2021-04-05 15:26:08 +1000

    Remove button reference in navbr

--- config/index.js
+++ config/index.js
@@ -90,13 +90,7 @@ module.exports = {
         name: "Get in touch",
         url: "/#contact",
       },
-    ],
-    button: {
-      useFileName: true,
-      name: "Resume",
-      fileName: "goldenpuppy.pdf", // the file has to be placed inside the static folder at the root level
-      url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
-    },
+    ]
   },
   footerLinks: [
     {
