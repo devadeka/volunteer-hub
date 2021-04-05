bc23646d4d985c8c57b81dccf6a4371a9c9bc5d3
Stephen Watts <sgwatts96@gmail.com>
2021-04-05 15:27:02 +1000

    Remove reference to button in nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -34,27 +34,12 @@ const StyledNav = styled.nav`
     &:hover::before {
       width: 100%;
     }
-  }
-  .cta-btn {
-    width: auto;
-    height: auto;
-    font-weight: 700;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    border: 0.125rem solid ${({ theme }) => theme.colors.primary};
-    background: ${({ theme }) => theme.colors.background};
-    transition: 20ms ease-out;
-    font-size: 1rem;
-    padding: 0.5rem 1.5rem;
-    margin: 0;
-    &:hover {
-      background: ${({ theme }) => theme.colors.primary};
-      color: ${({ theme }) => theme.colors.background};
     }
   }
 `
 
 const Navbar = () => {
-  const { menu, button } = navLinks
+  const { menu } = navLinks
   return (
     <StyledNav>
       {menu.map(({ name, url }, key) => {
@@ -64,20 +49,6 @@ const Navbar = () => {
           </Link>
         )
       })}
-      {button.useFileName ? (
-        <a
-          className="cta-btn"
-          href={`/${button.fileName}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {button.name}
-        </a>
-      ) : (
-        <Link className="cta-btn" to={button.url}>
-          {button.name}
-        </Link>
-      )}
     </StyledNav>
   )
 }
